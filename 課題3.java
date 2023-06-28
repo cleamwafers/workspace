ded1ec9adea1b6e8922718b2c24da0f6ced9ff45
cleamwafers <cleamwafers@yahoo.co.jp>
2023-06-28 20:43:57 +0900

    SLスタジオ_課題３

---
+++ 課題3.java
@@ -0,0 +1,12 @@
+public class 課題3 {
+     public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 5;
+
+        System.out.println(number1 + number2);
+        System.out.println(number1 - number2);
+        System.out.println(number1 * number2);
+        System.out.println(number1 / number2);
+        System.out.println(number1 % number2);
+    }
+}
