a3e3afaed492f22dbd956994838143253bfff0bf
cleamwafers <cleamwafers@yahoo.co.jp>
2023-06-28 18:07:03 +0900

    SLスタジオ初めての課題

---
+++ 課題.java
@@ -0,0 +1,10 @@
+public class 課題 {
+    public static void main(String[] args){
+    int number = 5;
+    String text = "あああ";
+   /*boolean flag = true;*/
+
+    System.out.println(number);
+    System.out.println(text);
+    }
+}
---
+++ 課題2.java
@@ -0,0 +1,11 @@
+public class 課題2 {
+     public static void main(String[] args) {
+        int number = 5;
+        String text = "ああああ";
+        /*boolean flag = true;*/
+
+        System.out.println(number);
+        System.out.println(text);
+
+    }
+}
