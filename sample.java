232b7498dabaca8d4d18878c668a5f843927c0a6
cleamwafers <cleamwafers@yahoo.co.jp>
2023-06-28 17:25:44 +0900

    first commit

---
+++ sample.java
@@ -0,0 +1,11 @@
+class Sample
+{
+    public static void main(String[]args)
+    {
+        String text ="ok!";
+        System.out.println(text);
+
+        String text2 ="ng!";
+        System.out.print(text2);
+    }
+}
